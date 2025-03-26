3eb9705b74ffb33aa74c9632607db93b3a3833b1
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-03-26 09:20:23 -0300

    :arrow_up: inclusão da documentação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 3000;
+const ApiRoute = process.env.API_ROUTE || 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Api sisnato')
+    .setDescription('Documentação da api SisNato')
+    .setVersion('2.0')
+    .addServer(`http://localhost:${port}`, 'Servidor Local')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(ApiRoute, app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(port).then(() => {
+    console.log('');
+    console.log(`Server running on http://localhost:${port}/`);
+    console.log(`Api running on http://localhost:${port}/${ApiRoute}/`);
+  });
 }
 bootstrap();
