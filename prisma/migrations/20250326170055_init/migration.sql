90b8c1e5a4635cd51bb1dfd69f515626dc41f775
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-03-26 14:01:06 -0300

    Create migration.sql

---
+++ prisma/migrations/20250326170055_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Logs" (
+    "id" SERIAL NOT NULL,
+    "User" INTEGER NOT NULL,
+    "EffectId" INTEGER NOT NULL,
+    "EffectTable" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Logs_pkey" PRIMARY KEY ("id")
+);
