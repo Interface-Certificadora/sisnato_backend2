01ecc7cde3f90d0699c2ca5bbf6d22af6ddc8900
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-03-26 11:37:09 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 // import { SeModule } from './se/se.module';
 import { S3Module } from './s3/s3.module';
 import { UserModule } from './api/user/user.module';
+import { AlertsModule } from './api/alerts/alerts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, S3Module, UserModule],
+  imports: [PrismaModule, S3Module, UserModule, AlertsModule, AuthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Sis Nato Api')
+    .setDescription('Api de integração com o Sis Nato')
+    .setVersion('1.0')
+    .addTag('Rotas')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  await app.listen(3000, () => {
+    console.log(` `);
+    console.log(` `);
+    console.log(` `);
+    console.log(`Nest running on http://localhost:${3000}`);
+    console.log(` `);
+  });
 }
 bootstrap();
