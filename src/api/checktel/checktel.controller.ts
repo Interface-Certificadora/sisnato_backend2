263d9006012cfb66d306c6d4aa656b9f98815d87
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-04-02 14:53:52 -0300

    feat rotas checktel get infos

--- src/api/checktel/checktel.controller.ts
+++ src/api/checktel/checktel.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ChecktelService } from './checktel.service';
 import { CreateChecktelDto } from './dto/create-checktel.dto';
 import { UpdateChecktelDto } from './dto/update-checktel.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { Checktel } from './entities/checktel.entity';
+import { ErrorChecktelEntity } from './entities/checktel.error.entity';
 
 @Controller('checktel')
 export class ChecktelController {
   constructor(private readonly checktelService: ChecktelService) {}
 
-  @Post()
-  create(@Body() createChecktelDto: CreateChecktelDto) {
-    return this.checktelService.create(createChecktelDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.checktelService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.checktelService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateChecktelDto: UpdateChecktelDto) {
-    return this.checktelService.update(+id, updateChecktelDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.checktelService.remove(+id);
+  @Get('/:tell')
+  @ApiResponse({
+    status: 200,
+    description: 'Verificado com sucesso',
+    type: Checktel,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Erro interno',
+    type: ErrorChecktelEntity,
+  })
+  async getTell(@Param('tell') tell: string) {
+    return await this.checktelService.getTell(tell);
   }
 }
