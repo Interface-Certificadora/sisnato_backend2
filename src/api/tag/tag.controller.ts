9ee3864f2c1d98f80c254c9cb78c27356709d714
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-04-04 09:59:10 -0300

    Update tag.controller.ts

--- src/api/tag/tag.controller.ts
+++ src/api/tag/tag.controller.ts
@@ -1,33 +1,123 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TagService } from './tag.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { TagEntity } from './entities/tag.entity';
 
 @Controller('tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Cria uma tag',
+    description: 'Endpoint para criar uma tag',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Tag criada com sucesso',
+    type: TagEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao criar tag',
+    example: {
+      message: 'Erro ao criar tag',
+    },
+  })
   create(@Body() createTagDto: CreateTagDto) {
     return this.tagService.create(createTagDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Retorna todas as tags',
+    description: 'Endpoint para retornar todas as tags',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna todas as tags',
+    type: [TagEntity],
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao buscar tags',
+    example: {
+      message: 'Erro ao buscar tags',
+    },
+  })
   findAll() {
     return this.tagService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Retorna uma tag pelo id',
+    description: 'Endpoint para retornar uma tag pelo id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Retorna uma tag pelo id',
+    type: TagEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao buscar tag pelo id',
+    example: {
+      message: 'Erro ao buscar tag pelo id',
+    },
+  })
   findOne(@Param('id') id: string) {
     return this.tagService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualiza uma tag pelo id',
+    description: 'Endpoint para atualizar uma tag pelo id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Atualiza uma tag pelo id',
+    type: TagEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao atualizar tag pelo id',
+    example: {
+      message: 'Erro ao atualizar tag pelo id',
+    },
+  })
   update(@Param('id') id: string, @Body() updateTagDto: UpdateTagDto) {
     return this.tagService.update(+id, updateTagDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Remove uma tag pelo id',
+    description: 'Endpoint para remover uma tag pelo id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Remove uma tag pelo id',
+    type: TagEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro ao remover tag pelo id',
+    example: {
+      message: 'Erro ao remover tag pelo id',
+    },
+  })
   remove(@Param('id') id: string) {
     return this.tagService.remove(+id);
   }
