7d74c401da94bbcb9805b54ff97bc268259d4564
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-04-22 08:38:20 -0300

    dashboard

--- src/api/dashboard/entities/dashboard.entity.ts
+++ src/api/dashboard/entities/dashboard.entity.ts
@@ -1,10 +1,78 @@
 import { ApiResponseProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 
-export class Dashboard {
-  @ApiResponseProperty({ type: Number })
+export class ContagemItem {
+  @ApiResponseProperty()
   @Expose()
-  id: number;
+  ano: number;
+
+  @ApiResponseProperty()
+  @Expose()
+  mes: number;
+
+  @ApiResponseProperty()
+  @Expose()
+  total: number;
+
+  @ApiResponseProperty({ type: [Number] })
+  @Expose()
+  solicitacoes: number[];
+
+  @ApiResponseProperty()
+  @Expose()
+  mediaHoras: string;
+
+  @ApiResponseProperty()
+  @Expose()
+  videoConferencia: number;
+
+  @ApiResponseProperty()
+  @Expose()
+  interna: number;
+
+  @ApiResponseProperty()
+  @Expose()
+  RG: number;
+
+  @ApiResponseProperty()
+  @Expose()
+  CNH: number;
+}
+
+// Classe para cada tag
+export class TagItem {
+  @ApiResponseProperty()
+  @Expose()
+  descricao: string;
+
+  @ApiResponseProperty()
+  @Expose()
+  quantidade: number;
+}
+
+// Classe para a estrutura de tags
+export class Tags {
+  @ApiResponseProperty()
+  @Expose()
+  total_tags: number;
+
+  @ApiResponseProperty({ type: [TagItem] })
+  @Expose()
+  @Type(() => TagItem)
+  lista_tags: TagItem[];
+}
+
+// Classe principal do dashboard
+export class Dashboard {
+  @ApiResponseProperty({ type: [ContagemItem] })
+  @Expose()
+  @Type(() => ContagemItem)
+  contagem: ContagemItem[];
+
+  @ApiResponseProperty({ type: Tags })
+  @Expose()
+  @Type(() => Tags)
+  tags: Tags;
 
   constructor(partial: Partial<Dashboard>) {
     Object.assign(this, partial);
--- src/api/dashboard/utils/entities/utils.entity.ts
+++ src/api/dashboard/utils/entities/utils.entity.ts
@@ -2,3 +2,22 @@ export class MesesEntity {
   mes: number;
   ano: number;
 }
+
+export class solicitacoesEntity {
+  id: number;
+  createdAt: Date;
+  andamento: string;
+  type_validacao: String;
+  dt_aprovacao: Date | null;
+  hr_aprovacao: Date | null;
+  dt_agendamento: Date;
+  hr_agendamento: Date;
+  id_fcw: Number;
+  uploadCnh: string | null;
+  uploadRg: string | null;
+  dt_aprovacao_formatada: string;
+  hr_aprovacao_formatada: string;
+  dt_agendamento_formatada: string;
+  hr_agendamento_formatada: string;
+  typeDoc: string;
+}
