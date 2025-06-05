a9e1bfb3b75e9e5a968338277012e0e84274570c
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-05 08:29:45 -0300

    Update alert.service.ts

--- src/api/alert/alert.service.ts
+++ src/api/alert/alert.service.ts
@@ -68,6 +68,7 @@ export class AlertService {
             User.hierarquia !== 'ADM' && {
               corretor: {
                 id: User.id,
+                status: true,
               },
             }),
         },
