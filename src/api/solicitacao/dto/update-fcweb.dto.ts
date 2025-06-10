5c4280d5efc4f0e499b8be0dd7023b53073f3410
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-10 16:37:36 -0300

    correção para atualizar a fcweb

---
+++ src/api/solicitacao/dto/update-fcweb.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+
+
+export class UpdateFcwebDto {
+  @ApiProperty({
+    example: 1,
+    description: 'atualização d ficha de cadastro',
+    required: true,
+  })
+  @IsNumber({},{ message: 'id_fcw deve ser um número' })
+  @IsNotEmpty({ message: 'id_fcw deve ser informado' })
+  id_fcw: number;
+
+  @ApiProperty({
+    example: 'APROVADO',
+    description: 'atualização d ficha de cadastro',
+    required: true,
+  })
+  @IsString({ message: 'andamento deve ser uma string' })
+ @IsOptional()
+  andamento: string;
+}
